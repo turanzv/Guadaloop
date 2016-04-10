9b685e3047f3c9d979902e049e2fcc77d2e0426d
tzvural <tzvural@utexas.edu>
2016-04-10 00:06:47 -0500

    First commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/parallels/guadaloop_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/parallels/guadaloop_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/parallels/guadaloop_ws/devel/lib:/home/parallels/guadaloop_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/parallels/guadaloop_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/parallels/guadaloop_ws/devel/lib/pkgconfig:/home/parallels/guadaloop_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/parallels/guadaloop_ws/build"
+export PYTHONPATH="/home/parallels/guadaloop_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/parallels/guadaloop_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/parallels/guadaloop_ws/src:$ROS_PACKAGE_PATH"
